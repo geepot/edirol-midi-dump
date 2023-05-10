6a372f49ddece786fadeab0fdde50b5b25bc7c94
geepot <69025839+geepot@users.noreply.github.com>
2023-05-09 21:26:17 -0700

    feat: add automated dump, comparison, and logging
    Overhaul mididump_compare.py to dump, automatically compare,
    and save a CSV file with user provided notes
    
    Deprecate original scripts and Touchdesigner project
    
    Add requirements.txt to ensure future compatibility
    
    Change to uppercase hexadecimal to distinguish between hex and dec
    
    Update .gitignore to ignore script generated files

--- scripts/mididump_compare.py
+++ scripts/mididump_compare.py
@@ -1,73 +1,131 @@
-# This script takes two pasted MIDI Bulk Dumps in the format of example_dump.txt and outputs the changed bytes and their addresses.
-# If you prefer uppercase hexidecimal, make the changes to lines 67 and 73 as noted in the comments.
+import mido
+import csv
+from collections import OrderedDict
 
-import sys
+def get_midi_device():
+    input_names = mido.get_input_names()
+    print("Available MIDI devices:")
+
+    for i, name in enumerate(input_names, start=1):
+        print(f"{i}. {name}")
+
+    device_index = int(input("Enter the number of the MIDI device you'd like to use: ")) - 1
+
+    return mido.open_input(input_names[device_index])
+
+def get_midi_dump(port):
+    midi_dump = ""
+    raw_dump = []
+    print("Waiting for MIDI dump...")
+    while True:
+        msg = port.receive()
+        # print("[DEBUG] Received MIDI message:", msg)
+        if msg.type == 'sysex':
+            hex_msg = ''.join(f"{byte:02X}" for byte in msg.data)
+            print(hex_msg)
+            if hex_msg == '417F000028122F7F000052':
+                break
+            midi_dump += hex_msg + "\n"  # Add newline after each message
+    return midi_dump
+
+def send_midi_dump(raw_dump, midi_port): # This is definitely still broken
+    for msg in raw_dump:
+        message = mido.Message.from_hex(msg)
+        print(message)
+    
+    # print('sysex', bytes.from_hex("417F000028122F7F000052"))
+    # midi_port.send(mido.Message.from_hex())
 
 def parse_midi_dump(midi_dump):
-    discarded_string = "f0417f417f0000002812"
-    if midi_dump.startswith(discarded_string):
-        midi_dump = midi_dump[len(discarded_string):]
+    dump_lines = midi_dump.strip().split("\n")
+    parsed_lines = []
 
-    starting_address = midi_dump[:6]
-    midi_dump = midi_dump[6:]
+    for line in dump_lines:
+        if not line.startswith("417F00002812"):
+            continue
+        parsed_line = []
+        for i in range(12, len(line) - 2, 2):
+            parsed_line.append(int(line[i : i + 2], 16))
+            # print("[DEBUG] parsed_line: ", parsed_line)
+        parsed_lines.append(parsed_line)
 
-    end_of_message_marker = "f7"
-    end_of_message_index = midi_dump.find(end_of_message_marker)
+    return parsed_lines
 
-    checksum = midi_dump[end_of_message_index - 2:end_of_message_index]
-    data = midi_dump[:end_of_message_index - 2]
-
-    return starting_address, data, checksum
-
-
-def parse_midi_dump_input(input_lines):
-    memory_map = {}
-    for midi_dump in input_lines:
-        starting_address, data, checksum = parse_midi_dump(midi_dump)
-        address = int(starting_address, 16)
-        data_bytes = [data[i:i+2] for i in range(0, len(data), 2)]
-        for byte in data_bytes:
-            memory_map[address] = byte
+def map_memory_space(parsed_dump):
+    memory_space = OrderedDict()
+    for line in parsed_dump:
+        # print("[DEBUG] Line: ", line)
+        address = (line[0] << 16) | (line[1] << 8) | line[2] # Calculate starting address based on first three bytes of the line
+        # print("[DEBUG] Address: ", hex(address))
+        for value in line[3:-1]:
+            memory_space[address] = value
             address += 1
-    return memory_map
+    return memory_space
 
-def input_midi_dump():
-    input_lines = []
-    print("Please paste the MIDI dump and press Enter. The program will process the input once it encounters the end-of-dump marker:")
-    while True:
-        line = input().strip()
-        input_lines.append(line)
-        if line == 'f0417f417f00000028122f7f000052f7':
-            break
-    return input_lines
+def save_setting_to_csv(setting_name, addresses, original_values, min_values, max_values, special_notes, filename="settings.csv"):
+    with open(filename, "a", newline="") as csvfile:
+        fieldnames = ["Setting", "Address", "Original", "Minimum", "Maximum", "Special Notes"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-def compare_memory_maps(map1, map2):
-    differing_addresses = set(map1.keys()) & set(map2.keys())
-    differences = []
-    for address in differing_addresses:
-        if map1[address] != map2[address]:
-            differences.append((address, map1[address], map2[address]))
-    return differences
+        for address, original, minimum, maximum, note in zip(addresses, original_values, min_values, max_values, special_notes):
+            writer.writerow({
+                "Setting": setting_name,
+                "Address": f"{address:06X}",
+                "Original": f"{original:02X}",
+                "Minimum": f"{minimum:02X}",
+                "Maximum": f"{maximum:02X}",
+                "Special Notes": note
+            })
 
-print("Enter the first MIDI dump:")
-midi_dump_lines1 = input_midi_dump()
-memory_map1 = parse_midi_dump_input(midi_dump_lines1)
+# Main program
+midi_port = get_midi_device()
 
-print("Enter the second MIDI dump:")
-midi_dump_lines2 = input_midi_dump()
-memory_map2 = parse_midi_dump_input(midi_dump_lines2)
+print("\nDo not modify any settings, this dump will be used as a baseline for comparison.")      
+original_dump = get_midi_dump(midi_port)
 
-differences = compare_memory_maps(memory_map1, memory_map2)
+original_memory_space = map_memory_space(parse_midi_dump(original_dump))
 
-if not differences:
-    print("No differences found between the two memory maps.")
-else:
-    print("Differences found:")
-    for address, value1, value2 in differences:
-        print(f"Address: {address:04x}, Values: {value1} - {value2}") # Change '04x' to '04X' for uppercase
+while True:
+    setting_name = input("Enter the name of the setting you want to record: ")
+    print("\nPlease set the setting at its minimum value.")
+    min_value_dump = get_midi_dump(midi_port)
+    min_memory_space = map_memory_space(parse_midi_dump(min_value_dump))
 
-    setting_name = input("Enter the name of the setting: ")
+    print("\nPlease set the setting at its maximum value.")
+    max_value_dump = get_midi_dump(midi_port)
+    max_memory_space = map_memory_space(parse_midi_dump(max_value_dump))
 
-    print(f"Address location and data value for '{setting_name}':") # Todo: make this output and append to a text file
-    for address, value1, value2 in differences:
-        print(f"Address: {address:04x}, Values: {value1} - {value2}") # Change '04x' to '04X' for uppercase
+    changed_addresses = []
+    original_values = []
+    min_values = []
+    max_values = []
+    
+    for address in original_memory_space.keys():
+        if original_memory_space[address] != min_memory_space[address] or original_memory_space[address] != max_memory_space[address]:
+            changed_addresses.append(address)
+            original_values.append(original_memory_space[address])
+            min_values.append(min_memory_space[address])
+            max_values.append(max_memory_space[address])
+
+    special_notes = []
+    for address, original, minimum, maximum in zip(changed_addresses, original_values, min_values, max_values):
+        print(f"Address: {address:06X}, Original: {original:02X}, Minimum: {minimum:02X}, Maximum: {maximum:02X}")
+        note = input("Enter any special notes for this address (leave empty if none): ")
+        special_notes.append(note)
+
+    print("\nSaving the following information to the CSV file:")
+    print("Setting:", setting_name)
+    for address, original, minimum, maximum, note in zip(changed_addresses, original_values, min_values, max_values, special_notes):
+        print(f"Address: {address:06X}, Original: {original:02X}, Minimum: {minimum:02X}, Maximum: {maximum:02X}, Special Notes: {note}")
+
+    correct = input("Is this information correct? (y/n): ")
+    if correct.lower() == 'y':
+        save_setting_to_csv(setting_name, changed_addresses, original_values, min_values, max_values, special_notes)
+
+    another_setting = input("Do you want to record another setting? (y/n): ")
+    if another_setting.lower() != "y":
+        ### BROKEN ###
+        # write_back = input("Do you want to write back your original settings? (y/n): ")
+        # if write_back.lower() == "y":
+        #     send_midi_dump(original_dump, midi_port)
+        break
